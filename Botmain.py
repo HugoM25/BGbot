550868f6b9841894f1fc1d42b5e6fb32210185f5
HugoM25 <58184548+HugoM25@users.noreply.github.com>
2020-11-05 17:33:13 +0100

    Update Botmain.py
    Ajout de la fonction opendoc

--- Botmain.py
+++ Botmain.py
@@ -29,11 +29,13 @@ class Bot(discord.Client):
             Messagehelp += "------Utile------\n"
             Messagehelp += "**!bgbot poll [Votre texte du sondage]**\n"
             Messagehelp += "*Permet de créer des sondages.*\n"
+            Messagehelp += "**!bgbot opendoc [add/show/clear]**\n"
+            Messagehelp += "*Gère le fichier commun, tout le monde peut y écrire, parfait pour les sondages !*\n"
             Messagehelp += "------Divers------\n"
             Messagehelp += "**!bgbot blague**\n"
             Messagehelp += "*je ne suis pas l'auteur de ces blagues, elles peuvent donc être très nulles.*\n"
             Messagehelp += "**!bgbot emojimage [Le lien de votre image ou alors en pièce jointe]**\n"
-            Messagehelp += "*Encore en bêta mais est censé changer votre image en emoji.*"
+            Messagehelp += "*Encore en bêta mais est censé changer votre image en emoji.*\n"
             await message.channel.send(Messagehelp)
 
 
@@ -54,6 +56,7 @@ class Bot(discord.Client):
 
             await message.channel.send( "[POLL] " + pollmessage)
 
+        #Renvoie une blague à l'utilisateur (parfois douteuse, le .json de blague n'est pas de moi)----------------------
         if (message.content.startswith(botcommand + "blague")):
             message_blague = ""
             with open("blagues.json", encoding="utf-8")as f:
@@ -61,6 +64,36 @@ class Bot(discord.Client):
                 randomblague = random.randint(0,len(jsonData))
                 message_blague = str(jsonData[randomblague]["joke"])+ " " +  str(jsonData[randomblague]["answer"])
             await message.channel.send(message_blague)
+
+        #Permet de gérer un open doc en commun (pratique pour récuperer des avis) --------------------------------------
+
+        #Ajoute un avis au document
+        if (message.content.startswith(botcommand + "opendoc add")):
+            messagetext = str(message.content)
+            messagelist = messagetext.split(" ")
+            messagetoadd = ""
+            for i in range(3, len(messagelist)):
+                messagetoadd += messagelist[i] + " "
+
+            with open("opendoc.txt", "a") as f :
+                f.write("------------- " + "\n")
+                f.write(messagetoadd)
+                f.write("\n" + str(message.author.name) +"\n")
+
+            await message.channel.send(message.author.mention +" Votre texte a bien été enregistré !")
+
+        #Donne le document
+        if (message.content.startswith(botcommand + "opendoc show")):
+            await message.channel.send("Voici votre document actuel :")
+            await message.channel.send(file=discord.File("opendoc.txt"))
+
+        if(message.content.startswith(botcommand+ "opendoc clear") and str(message.author) == "LeCoumik#0882"):
+            with open("opendoc.txt", "w") as f:
+                f.write("")
+            await message.channel.send("Le document a été vidé !")
+
+
+
 
 
 if __name__ == "__main__" :
